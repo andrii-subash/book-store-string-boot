9cec1480edbf0966aeabdbf0726f9928a1a3f5c7
Andrii <subashandrii@gmail.com>
2023-09-07 16:43:07 +0300

    added application

--- src/main/java/book/store/BookStoreApplication.java
+++ src/main/java/book/store/BookStoreApplication.java
@@ -1,13 +1,36 @@
 package book.store;
 
+import book.store.model.Book;
+import book.store.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+    private final BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
-	}
+    public BookStoreApplication(BookService bookService) {
+        this.bookService = bookService;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Kobzar");
+            book.setAuthor("Taras Shevchenko");
+            book.setIsbn("11233123");
+            book.setPrice(BigDecimal.valueOf(300));
+
+            bookService.save(book);
+            System.out.println(bookService.findAll());
+        };
+    }
 }
---
+++ src/main/java/book/store/service/impl/BookServiceImpl.java
@@ -0,0 +1,26 @@
+package book.store.service.impl;
+
+import book.store.model.Book;
+import book.store.repository.BookRepository;
+import book.store.service.BookService;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
